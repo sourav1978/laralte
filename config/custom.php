0bbcdf1e85bce56bf6a2578ae3ae9f0bf783bc34
Sourav Banerjee <souravbanerjee1978@gmail.com>
2019-03-01 00:51:53 +0530

    first commit

---
+++ config/custom.php
@@ -0,0 +1,11 @@
+<?php 
+return [
+    'facebook' => [
+        'url' => 'https://www.facebook.com/scotchdevelopment',
+        'username' => 'scotchdevelopment'
+    ],
+    'twitter' => [
+        'url' => 'https://twitter.com/scotch_io',
+        'username' => 'scotch_io'
+    ]
+];
